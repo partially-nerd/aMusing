106b416f8d0933a0895b448d62ad6119fb5fe3fe
partially-nerd <108736691+partially-nerd@users.noreply.github.com>
2023-04-21 12:05:55 +0545

    Update ytdlp.bat

--- dist/ytdlp.bat
+++ dist/ytdlp.bat
@@ -1,2 +1,2 @@
-cd ~\Desktop\CodeTheFuture\Day4\audio\
-~\Desktop\CodeTheFuture\Day4\dist\yt-dlp.exe -x --audio-format mp3 %2 --ffmpeg-location C:\Users\User\Documents\3Di\Pencil2D\plugins
+cd ~\Desktop\aMusing\Day4\audio\
+~\Desktop\aMusing\Day4\dist\yt-dlp.exe -x --audio-format mp3 %2 --ffmpeg-location C:\Users\User\Documents\3Di\Pencil2D\plugins
